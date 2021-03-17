78a464ff725a29bddbbc712f0d7f1c0ea38b7504
GuyInGrey <guyingreydaboss@gmail.com>
2021-03-17 10:05:05 -0600

    Changes

---
+++ MandelbrotRenderer/MandelbrotShader.cs
@@ -0,0 +1,70 @@
+﻿using ComputeSharp;
+
+namespace MandelbrotRenderer
+{
+    [AutoConstructor]
+    public readonly partial struct MandelbrotShader : IComputeShader
+    {
+        public readonly ReadWriteTexture2D<Rgba32, Float4> image;
+        public readonly Float4 viewport;
+        public readonly int maxIterations;
+        public readonly float power;
+
+        public void Execute()
+        {
+            var c = new Float2(
+                Hlsl.Lerp(ThreadIds.X / image.Width, viewport.X, viewport.W),
+                Hlsl.Lerp(ThreadIds.Y / image.Height, viewport.Y, viewport.Z));
+            var z = c;
+
+            var i = 0;
+            while (ComplexAbs(z) <= 2 && i < maxIterations)
+            {
+                z = ComplexPow(z, power) + c;
+                i++;
+            }
+
+            float final;
+            if (i == maxIterations) { final = i; }
+            else
+            {
+                final = i + 1 - Hlsl.Log(Hlsl.Log(ComplexAbs(z))) / Hlsl.Log(2);
+            }
+            final /= maxIterations;
+
+            image[ThreadIds.X, ThreadIds.Y].RGB = new Float3(final, final, final);
+        }
+
+        public static float ComplexAbs(Float2 complex)
+        {
+            var c = Hlsl.Abs(complex.X);
+            var d = Hlsl.Abs(complex.Y);
+            if (c > d)
+            {
+                var r = d / c;
+                return c * Hlsl.Sqrt(1f + r * r);
+            }
+            else if (d == 0)
+            {
+                return c;
+            }
+            else
+            {
+                var r = c / d;
+                return d * Hlsl.Sqrt(1f + r * r);
+            }
+        }
+
+        public static Float2 ComplexPow(Float2 val, Float2 pow)
+        {
+            if (pow.X == 0 && pow.Y == 0) { return Float2.One; }
+            if (val.X == 0 && val.Y == 0) { return Float2.Zero; }
+
+            var rho = ComplexAbs(val);
+            var theta = Hlsl.Atan2(val.Y, val.X);
+            var newRho = pow.X * theta + pow.Y * Hlsl.Log(rho);
+            var t = Hlsl.Pow(rho, pow.X) * Hlsl.Pow(2.71828182845f, -pow.Y * theta);
+            return new Float2(t * Hlsl.Cos(newRho), t * Hlsl.Sin(newRho));
+        }
+    }
+}
---
+++ MandelbrotRenderer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+using ComputeSharp;
+
+namespace MandelbrotRenderer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Starting...");
+            Run();
+            Console.WriteLine("Done!");
+            Console.Read();
+        }
+
+        public static unsafe void Run()
+        {
+            var bitmap = new Bitmap("blank.png");
+            var bitmapData = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format32bppArgb);
+
+            var viewport = new Float4(-2.25f, 0.75f, -1.5f, 1.5f);
+            var pow = 2f;
+            var maxIterations = 50;
+
+            try
+            {
+                var bitmapSpan = new Span<Rgba32>((Rgba32*)bitmapData.Scan0, bitmapData.Width * bitmapData.Height);
+                using var texture = Gpu.Default.AllocateReadWriteTexture2D<Rgba32, Float4>(bitmapSpan, bitmap.Width, bitmap.Height);
+                var instance = new MandelbrotShader(texture, viewport, maxIterations, pow);
+                Gpu.Default.For(bitmap.Width, bitmap.Height, instance);
+                texture.CopyTo(bitmapSpan);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+
+            bitmap.Save("Test.png", ImageFormat.Png);
+        }
+    }
+}
